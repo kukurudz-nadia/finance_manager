0afe11cb7e9f13779b6c792548fccc35dc7f8e99
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-02-17 01:14:06 +0200

    Add validations (#4)
    added validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :category
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :odate, presence: true
+  validates :description, presence: true
 end
