5cc884e4d26956ecfa2fd7251689a2361d2fc2f7
kukurudz-nadia <kukurudz.nadia6@gmail.com>
2023-02-16 22:29:09 +0200

    added models

---
+++ app/views/categories/_category.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! category, :id, :name, :descripton, :created_at, :updated_at
+json.url category_url(category, format: :json)
---
+++ db/schema.rb
@@ -0,0 +1,23 @@
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_202732) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "descripton"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.decimal "amount"
+    t.datetime "odate"
+    t.string "description"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+  end
+
+  add_foreign_key "transactions", "categories"
+end
